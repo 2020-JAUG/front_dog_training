a4b129b72fda753175489a1041298b1b791dde31
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-09-16 00:46:19 +0200

    Update the  navBar, && add function collapseOnSelect

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { connect } from "react-redux";
 import { LOGOUT } from "../../redux/types";
 import { useHistory } from "react-router-dom";
-import Training from '../Training/Training';
 import logo from "../../assets/Buenos_modales/pata1.png";
+import Navbar from "react-bootstrap/Navbar";
+import { Nav, NavDropdown } from "react-bootstrap";
 
 const Header2 = (props) => {
   let history = useHistory();
@@ -15,78 +16,170 @@ const Header2 = (props) => {
 
   if (props.credentials.user?.name) {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top header">
-        <div className="container-fluid">
-            <img onClick={() => history.push("/")} className="logoHeader" src={logo} alt="logo" height="78em" width="76"/>
-          <button
-            className="navbar-toggler burguer"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#toggleMobileMenu"
-            aria-controls="toggleMobileMenu"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="toggleMobileMenu">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0 text-center" Style="cursor:pointer;">
-              <li className="nav-item" Style="cursor:pointer;">
-              <div className="text-center linkLogout mt-2 header" onClick={() => history.push("/commonwall")}>COMUNIDAD&nbsp; &nbsp;</div>
-              </li>
-              <li className="nav-item text" Style="cursor:pointer;">
-              <div className="text-center linkLogout mt-2" onClick={() => history.push("/contact")}>CONTÁCTANOS&nbsp; &nbsp;</div>
-              </li>
-              <li className="nav-item">
-              </li>
-                <Training />&nbsp; &nbsp;
-
-            </ul>
-                <div className="text-center linkLogout" Style="cursor:pointer;" onClick={() => history.push("/profile")}  >{props.credentials?.user.name} &nbsp; &nbsp; </div>
-                <span>&nbsp; &nbsp;</span>
-                <div className="linkLogout text-center" onClick={() => logOut()}>LOGOUT</div>
-          </div>
-        </div>
-      </nav>
+      <>
+        <Navbar
+          collapseOnSelect
+          expand="lg"
+          bg="white"
+          variant="dark"
+          fixed="top"
+          className="text-center"
+        >
+          <Navbar.Brand href="">
+            <img
+              onClick={() => history.push("/")}
+              src={logo}
+              height="78em"
+              width="76"
+              className="d-inline-block align-top logoHeader"
+              alt="logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" className="m-2" id="bgInfo"/>
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link
+                href="#features"
+                onClick={() => history.push("/commonwall")}
+                id="linkLogout"
+              >
+                COMUNIDAD
+              </Nav.Link>
+              <Nav.Link
+                href="#features"
+                onClick={() => history.push("/contact")}
+                id="linkLogout"
+              >
+                CONTÁCTANOS
+              </Nav.Link>
+              <NavDropdown title="ADIESTRAMIENTO" id="collasible-nav-dropdown">
+                <NavDropdown.Item
+                  className="text-center dropdown"
+                  onClick={() => history.push("/trainers")}
+                  href="#action/3.1"
+                >
+                  Adiestradores
+                </NavDropdown.Item>
+                <NavDropdown.Item
+                  className="text-center dropdown"
+                  onClick={() => history.push("/buenosmodales")}
+                  href="#action/3.2"
+                >
+                  Buenos Modales
+                </NavDropdown.Item>
+                <NavDropdown.Item
+                  className="text-center dropdown"
+                  onClick={() => history.push("/ejercicios")}
+                  href="#action/3.3"
+                >
+                  Ejercicios
+                </NavDropdown.Item>
+                <NavDropdown.Item
+                  className="text-center dropdown"
+                  onClick={() => history.push("/obedienciabasica")}
+                  href="#action/3.4"
+                >
+                  Obediencia Básica
+                </NavDropdown.Item>
+                <NavDropdown.Item
+                  className="text-center dropdown"
+                  onClick={() => history.push("/alimentacion")}
+                  href="#action/3.4"
+                >
+                  Salud
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link
+                href="#deets"
+                onClick={() => history.push("/profile")}
+                id="linkLogout"
+              >
+                {props.credentials?.user.name} &nbsp; &nbsp;
+              </Nav.Link>
+              <Nav.Link
+                eventKey={2}
+                href="#memes"
+                id="linkLogout"
+                onClick={() => logOut()}
+              >
+                SIGN OUT
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </>
     );
   } else {
-    return(
-    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top header" Style="border: none;">
-        <div className="container-fluid">
-            <img onClick={() => history.push("/")} className="logoHeader" src={logo} alt="logo" height="78em" width="76"/>
-          <button
-            className="navbar-toggler burguer"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav text-center me-auto mb-2 mb-lg-0">
-              <li className="nav-item" Style="cursor:pointer;">
-              <div className="text-center mt-3 about linkLogout" onClick={() => history.push("/aboutus")}>SOBRE NOSOTROS&nbsp; &nbsp;</div>
-              </li>
-              <li><a href="{}"><div id="google_translate_element"></div></a></li>
-              <li className="nav-item" Style="cursor:pointer;">
-              <div className="text-center mt-3 about linkLogout" onClick={() => history.push("/contact")}>CONTÁCTANOS&nbsp; &nbsp;</div>
-              </li>
-              <li className="nav-item about" Style="cursor:pointer;">
-                <div className="text-center mt-3 linkLogout" onClick={() => history.push("/trainers")}>ADIESTRADORES</div>
-              </li>
-
-            </ul>
-                <div className="text-center text-black about linkLogout" onClick={() => history.push("/login")} Style="cursor:pointer;">LOG IN</div>  &nbsp; &nbsp;
-                <div className="text-center text-black about linkLogout" onClick={() => history.push("/register")} Style="cursor:pointer;">SIGN UP</div>
-          </div>
-        </div>
-      </nav>
-    )
+    return (
+      <>
+        <Navbar
+          collapseOnSelect
+          expand="lg"
+          bg="light"
+          variant="dark"
+          fixed="top"
+          className="text-center"
+        >
+          <Navbar.Brand href="">
+            <img
+              onClick={() => history.push("/")}
+              src={logo}
+              height="78em"
+              width="76"
+              className="d-inline-block align-top logoHeader"
+              alt="logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" className="m-2" id="bgInfo"/>
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link
+                href="#features"
+                onClick={() => history.push("/aboutus")}
+                id="linkLogout"
+              >
+                SOBRE NOSOTROS
+              </Nav.Link>
+              <Nav.Link
+                href="#features"
+                onClick={() => history.push("/contact")}
+                id="linkLogout"
+              >
+                CONTÁCTANOS
+              </Nav.Link>
+              <Nav.Link
+                href="#features"
+                onClick={() => history.push("/trainers")}
+                id="linkLogout"
+              >
+                ADIESTRADORES
+              </Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link
+                href="#deets"
+                onClick={() => history.push("/register")}
+                id="linkLogout"
+              >
+                SIGN UP &nbsp; &nbsp;
+              </Nav.Link>
+              <Nav.Link
+                eventKey={2}
+                href="#memes"
+                id="linkLogout"
+                onClick={() => history.push("/login")}
+              >
+                LOG IN &nbsp;&nbsp;
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </>
+    );
   }
-}
+};
 export default connect((state) => ({
   credentials: state.credentials,
 }))(Header2);

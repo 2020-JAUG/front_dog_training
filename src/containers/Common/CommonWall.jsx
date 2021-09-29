5302e1e3a85b39ca061b14f4ef367fb40c49c256
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-09-29 16:24:25 +0200

    Create an hooks to save the posts, instead of RDX

--- src/containers/Common/CommonWall.jsx
+++ src/containers/Common/CommonWall.jsx
@@ -1,39 +1,23 @@
-/* eslint-disable jsx-a11y/iframe-has-title */
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
-import { connect, useDispatch, useSelector } from "react-redux";
+import { connect } from "react-redux";
 import MakePost from "../../components/MakePost/MakePost";
 import Post from "../../components/Post/Post";
-
-//ACTIONS OF RDX
-import { getPostAction } from "../../Actions/PostActions";
 import Spinner from "../../components/Spinner/Spinner";
-
-import axios from 'axios';
-import Swal from 'sweetalert2';
-import { GET_POST } from "../../redux/types";
-import { GET_POST_SUCCE } from '../../redux/types';
-import { ADD_POST_SUCCE } from '../../redux/types';
+import axios from "axios";
+import Swal from "sweetalert2";
+//RDX
+import { GET_POST_SUCCE } from "../../redux/types";
 
 const CommonWall = (props) => {
-    const [post, setPost] = useState([]);
+  const [post, setPost] = useState([]);
   const [datos] = useState({
     token: props.credentials?.token,
     user: props.credentials?.user,
   });
-  const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   //Consult the API
-  //   const findPost = (props) => dispatch(getPostAction(props));
-  //   findPost();
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
-
-    useEffect(() => {
-    //Consult the API
-    // const findPost = () => dispatch(getPostAction());
+  useEffect(() => {
     findPost();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const findPost = () => {
@@ -43,18 +27,15 @@ const CommonWall = (props) => {
     };
 
     axios
-      .get("http://localhost:5000/post", body, {
+      .get("https://jaug-dog-training.herokuapp.com/post", body, {
         headers: { authorization: "Bearer " + token },
       })
       .then((res) => {
         setPost(res.data);
-        console.log(res.data, 'res.data')
         props.dispatch({ type: GET_POST_SUCCE, payload: res.data });
       })
       .catch((err) => {
-        // console.log(err.response.data);
         console.log(err);
-        //But if there is an error, change the state
         //Alert error
         Swal.fire({
           icon: "error",
@@ -63,114 +44,31 @@ const CommonWall = (props) => {
         });
       });
   };
-  //Access to the states
-  // const post = useSelector((state) => state.data.post);
-  const loading = useSelector((state) => state.post.loading);
 
-  return (
-    <>
-         <MakePost />
-       <div className="commonWall">
-       {post[0]? (
-          post.reverse().map((post) => <Post key={post.id} post={post} />)
-        ) : (
-           <div className="spinnerContainer">
-            <Spinner />
-          </div>
-        )}
-        {/* {post.length === 0 ? (
-      <div className="spinnerContainer">
-        <Spinner />
-      </div>
-    ) : (
-      post.reverse().map((post) => <Post key={post.id} post={post} />)
-    )} */}
-      </div>
-
-
-
-
-
-
-
-
-    {/* <div className="commonWall">
+  if (post[0]?.id) {
+    return (
+      <div className="commonWall">
       <MakePost />
-      {loading ? (
-        <div className="spinnerContainer">
+        <div className="movieImage">
+          <div className="fondoIMage"></div>
+        </div>
+        <div className="movieContent">
+          {post.reverse().map((msg) => <Post key={msg.id} msg={msg} /> )}
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="spinnerContainer">
+        <div className="spinner">
           <Spinner />
         </div>
-      ) : null}
-      {post.length === 0 ? (
-        <div className="spinnerContainer">
-          <Spinner />
-        </div>
-      ) : (
-        [...post].reverse().map((post) => <Post key={post.id} post={post} />)
-      )}
-    </div> */}
-    </>
-  );
+      </div>
+    );
+  }
 };
 
 export default connect((state) => ({
   credentials: state.credentials,
   post: state.post,
 }))(CommonWall);
-
-
-
-
-
-
-
-
-
-// /* eslint-disable jsx-a11y/iframe-has-title */
-// import React, { useEffect } from "react";
-// import { connect, useDispatch, useSelector } from "react-redux";
-// import MakePost from "../../components/MakePost/MakePost";
-// import Post from "../../components/Post/Post";
-
-// //ACTIONS OF RDX
-// import { getPostAction } from "../../Actions/PostActions";
-// import Spinner from "../../components/Spinner/Spinner";
-
-// const CommonWall = (props) => {
-//   const dispatch = useDispatch();
-//   useEffect(() => {
-//     //Consult the API
-//     const findPost = (props) => dispatch(getPostAction(props));
-//     findPost();
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, []);
-
-//   //Access to the states
-//   // const post = useSelector((state) => state.post.post);
-//   const loading = useSelector((state) => state.post.loading);
-
-//   return (
-//     <>
-//       <div className="commonWall">
-//         <MakePost />
-//         {loading ? (
-//           <div className="spinnerContainer">
-//             <Spinner />
-//           </div>
-//         ) : null}
-//         {props.post.post.length === 0 ? (
-//           <div className="spinnerContainer">
-//             <Spinner />
-//           </div>
-//         ) : (
-//           props?.post.post.map((mensaje) => <Post key={mensaje.id} mensaje={mensaje} />)
-//         )}
-//       </div>
-//     </>
-//   );
-// };
-
-// export default connect((state) => ({
-//   credentials: state.credentials,
-//   post: state.post
-// }))(CommonWall);
